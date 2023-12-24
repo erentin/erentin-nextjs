f77ad811b6f308c9a7fe4a6017c1a5e892bad939
erentin <erentin@outlook.com>
2023-12-24 15:50:47 +0300

    nextjs

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,103 @@
+"use client";
+import { useState, useEffect } from 'react';
+import React from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+
+    const [opacity, setOpacity] = useState(0);
+    const [direction, setDirection] = useState(1);
+    const downloadUrl = 'http://localhost:3000/pdfs/eren-tin-english-cv-next-js.pdf';
+
+    const handleDownload = () => {
+        // Tarayıcıda dosyayı indirmek için bir bağlantı oluşturuluyor ve tıklanıyor.
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'eren-tin-cv.pdf'; // İndirilen dosyanın adını belirleyebilirsiniz.
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
+
+      useEffect(() => {
+        const interval = setInterval(() => {
+          // Opaklık değerini 0.1 birim artır veya azalt
+          setOpacity((prevOpacity) => {
+            if (prevOpacity >= 1) {
+              setDirection(-1);
+            } else if (prevOpacity <= 0.1) {
+              setDirection(1);
+            }
+            return prevOpacity + direction * 0.1;
+          });
+        }, 100); // Her 0.5 saniyede bir opaklık değiri değiştir
+    
+        return () => clearInterval(interval); // Component unmount olduğunda interval'ı temizle
+      }, [direction]);
+
+      const gradientStyle = {
+        background: `linear-gradient(to right, rgba(255, 0, 0, ${opacity}), rgba(0, 0, 255, ${1 - opacity})`,
+      };
+
+
+  return (
+    <section>
+        <div className='grid grid-cols-1 lg:grid-cols-12'>
+            <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <h1 className='text-white text-4xl lg:text-6xl font-extrabold py-4'>
+                <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-red-600 mb-4'>Hello, I'm </span><br/>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'Eren Tin.',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Web Developer.',
+                            1000,
+                            'PHP Developer.',
+                            1000,
+                            'Laravel Developer.',
+                            1000,
+                            'Backend Engineer.',
+                            1000,
+                            'A Coding Enthusiast.',
+                            1000,
+                        ]}
+                        wrapper="span"
+                        speed={10}
+                        repeat={Infinity}
+                    />
+                </h1>
+                    <p className='text-[#ADB7BE] text-base sm:text-lg  lg:text-xl mb-6'>
+                        I hold a bachelor's degree in Mathematics and Computer Science. 
+                        <br />
+                        My proficiency includes PHP, Laravel, Livewire, Tailwind, React.js, and JavaScript.
+                </p>
+                <div>
+                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 mb-4 bg-gradient-to-r from-red-400 to-blue-600 hover:bg-slate-200  text-white font-semibold'>Hire Me!</button>
+                    <button className='px-1 py-1 w-full sm:w-fit rounded-full mb-4 bg-transparent bg-gradient-to-r from-blue-400 to-red-600  text-white' 
+                    onClick={handleDownload}>
+                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+                    
+                    </button>
+                </div>
+            </div>
+            <div className="col-span-5 rounded-full place-self-center mt-4 lg:mt-0 bg-white p-[0.5px]" style={gradientStyle}>
+                <div className='rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[350px] lg:h-[350px] relative '>
+                    <Image 
+                        src="/images/hero/eren-memoji-png.png"
+                        alt='Eren Tin Memoji Image'
+                        className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                        width={250}
+                        height={250}
+                    />
+                </div>
+            </div>
+
+        </div>
+       
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState, useEffect  } from 'react'
+import Link from 'next/link'
+import NavLink from './NavLink'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+import MenuOverlay from './MenuOverlay';
+
+const NavLinks = [
+    {
+        title: "Clients",
+        path: "clients"
+    },
+    {
+        title: "About",
+        path: "about"
+    },
+    {
+        title: "Contact",
+        path: "contact"
+    }
+]
+
+
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    const handleResize = () => {
+        // Sayfa çözünürlüğü değiştiğinde yapılacak işlemleri buraya ekleyin
+        console.log('Sayfa çözünürlüğü değişti!');
+      };
+
+    useEffect(() => {
+        // Component ilk render edildiğinde bir kere çalışacak olan kod
+        handleResize();
+    
+        // window nesnesinin 'resize' olayını dinleyen event listener ekleniyor
+        window.addEventListener('resize', handleResize);
+    
+        // Component unmount olduğunda event listener'ı temizle
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []); // Bu etkileşim sadece bir kere gerçekleşir, sadece component ilk render olduğunda
+    
+
+  return (
+    <nav className='fixed  top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+        <div className='flex flex-wrap items-center justify-between mx-auto p-8 py-2'>
+            <Link href={"/"} className='text-lg md:text-5xl text-white font-semibold'>
+                L0GO
+            </Link>
+
+            <div className='mobile-menu block md:hidden'>
+                {
+                    !navbarOpen 
+                    ? 
+                        (
+                            <button onClick={() => setNavbarOpen(true)} className='flexitems-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                                <Bars3Icon className="h-5 w-5" />
+                            </button>
+                        )
+                    :
+                        (
+                            <button onClick={() => setNavbarOpen(false)} className='flexitems-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
+                                <XMarkIcon className="h-5 w-5" />
+                            </button>
+                        )
+                }
+            </div>
+            <div className='menu hidden md:block md:w-auto' id='navbar'>
+                <ul className='flex p-4 md:p-0 sm:flex-row md:space-x-8 mt-0'>
+                    {
+                        NavLinks.map( (link, index) => (
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+        {
+            navbarOpen ? 
+            (
+                <MenuOverlay links={NavLinks}/>
+            )
+            : null
+        }
+    </nav>
+  )
+}
+
+export default Navbar

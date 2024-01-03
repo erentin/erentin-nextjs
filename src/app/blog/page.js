341010695905607ea8474ffa2b33e023756d4103
Eren Tın <erentin@outlook.com>
2024-01-03 11:43:49 +0300

    import React Updated2.

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,6 +1,5 @@
 "use client";
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar'
 
 const blog = () => {

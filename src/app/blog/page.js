ee648b7d1e1e67be91e3809c17460a53851bc42e
Eren Tın <erentin@outlook.com>
2024-01-03 13:39:12 +0300

    test

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,18 +1,20 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar'
 
-const Blog = () => {
+const blog = () => {
+
   const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState({});
-
+  
   useEffect(() => {
+    console.log(loading)
+    setLoading(true)
     const fetchData = async () => {
       try {
-        setLoading(true);
-
         const response = await fetch('http://127.0.0.1:8000/api/blog');
-
+        
+        // response.ok kontrolü ile isteğin başarılı olup olmadığını kontrol edebilirsiniz
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
@@ -20,24 +22,77 @@ const Blog = () => {
         const data = await response.json();
         setPosts(data.data);
         console.log(data.data);
+      
       } catch (error) {
         console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
       }
+      
     };
 
+    setLoading(false)
     fetchData();
   }, []);
 
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <main className="flex justify-center min-h-screen flex-col bg-[#121212]">
-        {/* ... Geri kalan bileşen kodları ... */}
+        <div className='container mx-auto px-12 py-4 mt-[92px] sm:mt-[0px]'>
+
+          <section class="mx-auto max-w-screen-lg px-4 py-12">
+
+            <div class="mb-12 text-center">
+
+              <div class="mb-6 text-3xl sm:text-5xl font-semibold text-white">Latest Blog Posts</div>
+              <p class="text-white">I am sharing articles on my blog to revisit them in the future myself.</p>
+
+            </div>
+
+            <div class="grid gap-x-8 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
+
+        
+            {
+              Array.isArray(posts) && posts.length > 0 ? 
+              
+              (posts.map( (post) => 
+              
+              (
+                <article class="">
+                  <a href="#" class="flex flex-col rounded border bg-white transition hover:bg-gray-200"
+                    ><div class="">
+                      <img src="https://componentland.com/images/XpvTA1erbfhbzqnkxEyGX.png" class="w-full object-cover" />
+                    </div>
+                    <div class="flex-1 p-4">
+                      <p class="mb-4 text-xs font-bold">{post.main_title}</p>
+                      <h6 class="mb-4 text-xl font-light">{post.topic_title}</h6>
+                      <p class="mb-4 text-xs">{post.created_at_date} - <span class="mb-4 text-xs">{post.created_at_time}</span></p> 
+                      <div class="justify-starts mt-4 flex flex-wrap items-center">
+                      {Array.isArray(post.tags) && post.tags.length > 0 ? (
+                      <div class="mr-2 mt-1 rounded-2xl bg-blue-100 py-1.5 px-4 text-xs text-gray-600">#js</div>
+                      )
+                      : (null)
+                      }
+                    
+                    </div>
+                    </div>
+                    </a>
+                </article>
+              )
+
+
+                ))
+
+              : (null)
+            }
+
+            </div>
+
+          </section>
+
+        </div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Blog;
+export default blog
